b0131d1cd507b68674c154e9b3c445a7a545fe87
FabioSouto <38102388+FabioSouto@users.noreply.github.com>
2018-07-11 12:02:33 -0300

    Update Fábio P1.c

--- Fábio P1.c
+++ Fábio P1.c
@@ -1,55 +1,51 @@
+// Fï¿½bio Souto Santos 
+// 11621EAU019
+// PROGRAMA 1
 
-  /*    
-      Fábio Souto Santos
-      11621EAU019
-  */
+
+
 
 #include <stdio.h>
-#include <conio.h>
-
 int main()
 {
-	
-  int EST;
-  char bts[256];
-  EST = 0;
-  scanf("%s",&bts);
-
-    for(int i=0; bts[i] != '\0'; i++ )
+	int est=0, i=0, j=0;
+	char bits[256];
+	while (j>=0)
 	{
-    
-	    if(EST == 0 && bts[i] == '0')
-	
-		{
-    
-	    EST = 0;
-    
-	    }
-    
-	    else
-		{
-			
-        EST = 1;
-        	
+		j=0;
+		printf("Insira um numero valido para que seja determinado se este e multiplo 3: ");
+	    scanf("%s", bits);
+	    for (i=0; bits[i]!='\0'; i++)
+	    {
+	    	if (bits[i]!='0' && bits[i]!='1')
+	    	j++;
 		}
-
-    }
-    
-      printf("Estado = %d \n", EST);
-      printf("Seguimento originario = %s  ", bts);
-      if(EST == 0)
-	  {
-      
-	  printf("E' multiplo de 3!");
-
-      }
-	
-	  else
-	
-	  {
-    
-	  printf("Nao e' multiplo");
-    
-   	  }
-
-  }
+		j--;
+	}
+	for (i=0; bits[i]!='\0'; i++)
+	{
+		if (bits[i]=='0')
+		{
+			if (est==0)
+				est=0;
+			else if (est==2)
+			    est=1;
+			else
+			    est=2;
+		}
+		else if (bits[i]=='1')
+		{
+			if (est==2)
+			    est=2;
+			else if (est==0)
+			    est=1;
+			else
+			    est=0;
+		}
+	}
+	if (est==0)
+	    printf("\n%s e um multiplo de 3", bits);
+	else
+	    printf("\n%s nao um multiplo de 3", bits);
+	return 0;
+}
